b495f299c43c0a8ad1d77767067e6cc2e0d7e12b
1244325988@qq.com <1244325988@qq.com>
2021-04-29 23:50:03 +0800

    addpage: home detail

---
+++ lib/pages/detail_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget{
+
+  @override
+  State createState() {
+    return _DetailPageState();
+  }
+}
+
+class _DetailPageState extends State<DetailPage>{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
